9bef55718b6ec1d78c96e4408a929ce4106d39bd
Muhammet Sivri <muhammetsivri@gmail.com>
2020-02-14 21:51:03 -0500

    Update CameraSystem.cs

--- Services/CameraSystem.cs
+++ Services/CameraSystem.cs
@@ -1,9 +1,6 @@
-﻿using Unity.Burst;
-using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
-using Unity.Mathematics;
-using Unity.Physics;
+using Unity.Mathematics; 
 
 [AlwaysSynchronizeSystem]
 public class CameraSystem : JobComponentSystem
