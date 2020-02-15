0ee2894348ca283754eed626d3b9cfd51530cfba
Muhammet Sivri <muhammetsivri@gmail.com>
2020-02-14 21:44:47 -0500

    Update SelectableSystem.cs

--- Services/SelectableSystem.cs
+++ Services/SelectableSystem.cs
@@ -1,6 +1,5 @@
-﻿using Unity.Entities;
-using Unity.Jobs;
-using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs; 
 
 [UpdateAfter(typeof(SelectionSystem))]
 [AlwaysSynchronizeSystem]
@@ -10,17 +9,17 @@ public class SelectableComponentSystem : JobComponentSystem
     {
         var physicsWorldSystem = World.GetExistingSystem<Unity.Physics.Systems.BuildPhysicsWorld>();
         var collisionWorld = physicsWorldSystem.PhysicsWorld.CollisionWorld;
-        var managerGroup = GetEntityQuery(ComponentType.ReadOnly<ManagerSelection>());
-        var managerEntities = managerGroup.ToComponentDataArray<ManagerSelection>(Unity.Collections.Allocator.TempJob);
-        var managerSelection = managerEntities[0]; 
-        var managerSelectedEntity = managerSelection.HasSelection ? collisionWorld.Bodies[managerSelection.SelectionRigidBodyIndex].Entity : Entity.Null;
+        var managerQuery = GetEntityQuery(ComponentType.ReadOnly<ManagerSelection>());
+        var managerEntities = managerQuery.ToComponentDataArray<ManagerSelection>(Unity.Collections.Allocator.TempJob);
+        var managerEntity = managerEntities[0];
+        var selectedEntity = managerEntity.HasSelection ? collisionWorld.Bodies[managerEntity.SelectionRigidBodyIndex].Entity : Entity.Null;
 
         var handle = Entities.ForEach((Entity entity, ref Selectable selectable) =>
         {
-            if (!managerSelection.HasSelection || entity != managerSelectedEntity) selectable.IsSelected = false;
+            if (!managerEntity.HasSelection || entity != selectedEntity) selectable.IsSelected = false;
             else
                 selectable.IsSelected = true;
-            
+
         }).Schedule(inputDeps);
 
 
